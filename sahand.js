3a9f0173bcc5c50d2bc6b0520a2b462b0a47db67
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-04-14 12:50:01 +0300

    Update sahand.js

--- sahand.js
+++ sahand.js
@@ -1,17 +1,2 @@
-var x = prompt("enter 1 to conect website");
-
-if(x == 1){
-console.log('You conceted Website...');
-
-
-}
-else{
-window.location.href = 'index.html';
-
-}
-
-
-
-
 
 alert("Welcome ");
