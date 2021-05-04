5cad113ec28b0f758b7c9532182bfb668e2291c9
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-04 21:44:26 +0300

    Create Get.php

---
+++ Get.php
@@ -0,0 +1,32 @@
+<?php 
+
+$$_GET["user"];
+$$_GET["pass"];
+
+if($_GET["user"]==="333" && $_GET["pass"]==="271")
+{
+	echo "Complate Login";
+	
+}
+ 
+
+if(isset($_GET['Login']))
+{
+	
+
+	
+	echo"Login";
+	
+	
+	
+}
+
+
+
+elseif ($_GET["user"]=="sahand" && $_GET["pass"]=="wrya"){
+	echo "incorrct";
+}
+	
+
+ 
+ ?>
