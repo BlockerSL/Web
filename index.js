88bcc1655e9ae3a990e82b451e2b2c581f8c7acc
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-07 22:23:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,74 @@
   var  Tag=document.getElementById("Tag").value;
   if(Tag=="I"){
  
- S1();
+document.write("<div>
+  <style> 
+        table, th, td 
+        { 
+            border-collapse:collapse; 
+            border:1px solid black; 
+        } 
+        th, td 
+        { 
+            padding:5px; 
+        } 
+    </style> 
+    </head> 
+ 
+    <body> 
+ 
+    <table> 
+
+      <tr> 
+        <th style="background-color: #778899;">ناوی شێوگ</th> 
+        <th style="background-color: #778899;"> جۆرەکانی</th> 
+      </tr> 
+      <tr> 
+     
+        <td>ێۆدید</td> 
+        <td>Io<sup>-</sup> , Io<sub>2</sub><sup>-</sup> , Io<sub>3</sub><sup>-</sup> , Io<sub>4</sub><sup>-</sup></td> 
+      </tr> 
+<tr>
+
+<td>Io<sup>-</sup></td>
+<td> ژێر ێۆدیت </td>
+
+</tr>
+
+
+<tr>
+<td>Io<sub>2</sub><sup>-</sup></td>
+<td>ێۆدیت</td>
+</tr>
+   <tr>
+<td>Io<sub>3</sub><sup>-</sup></td>
+<td>ێۆدات</td>
+</tr>
+<tr>
+<td>Io<sub>4</sub><sup>-</sup></td>
+<td>پێر & ژوور  ێۆدات</td>
+</tr>
+
+    </table> 
+</center>
+
+</body>
+<style>
+
+body{
+
+background-attachment: fixed;
+background-position:center;
+background-repeat:no-repeat;
+
+}
+h1{
+border-radius:50px;
+box-shadow:0px 0px 10px 0px yellowgreen;
+
+}
+</style>
+</div>");
 
  
   }
