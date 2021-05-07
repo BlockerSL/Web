44b570939a14ca3e8eaa960b5892c52e27c21149
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-07 21:59:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
   if(Tag=="I"){
  
 
-window.location.href = 'Sahand.html';
+S1()
 
  
   }
@@ -13,4 +13,24 @@ window.location.href = 'Sahand.html';
     alert("fail");
   }
   
+}
+function S1()
+{
+var users = ['Bro<sup>-</sup> ', 'Bro<sub>2</sub><sup>-</sup>', 'Bro<sub>3</sub><sup>-</sup>','Bro<sub>4</sub><sup>-</sup>'];
+    var ages = ['ژوور بڕۆمات','بڕۆمات','بڕۆمیت','ژێر بڕۆمیت'];
+
+    // Getting output element
+    var output = document.getElementById('output');
+
+    // Creating table tags
+    var table = "<table><thead><tr><th>ID</th><th>Name</th><th>Age</th></tr></thead><tbody>";
+
+    for (var i = 0; i < users.length; i++) {
+      table += "<tr><td>" + (i + 1) + "</td><td>" + users[i] + "</td><td>" + ages[i] + "</td><td>"  + "</td></tr>";
+    }
+
+    table += "</tbody></table>";
+
+    // Binding output element with table var
+    output.innerHTML = table;
 }
