6d29f24fbb41accb4fb977c21f285f09da615ac0
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-07 18:11:06 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+    function sahand()
+{
+  var  Tag=document.getElementById("Tag").value;
+  if(Tag=="I"){
+  document.write("welcome");
+  return false;
+  
+  }
+  
+  else{
+    alert("fail");
+  }
+  
+}
