0025e24cdd28c3b910964e963f90bc0562a915db
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-07 23:50:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
   if(Tag=="I"){
  
 
-   document.write(displayInventory(table));
+   document.write(Sahand());
 
  
   }
@@ -18,22 +18,21 @@
 
 
 
-function displayInventory(table){<!--from w w  w. j a  va 2  s  . c  o  m-->
+function Sahand(){
+  
+
 document.write("<TABLE BORDER=ON>");
 document.write("<TH>Item Number</TH><TH>Item Name</TH><TH>Model Number</TH><TH>Quantity</TH>");
-for(x=1; x<=3; x++) {
-document.write("<TR><TD>",x,"</TD>");
-for(y=0; y<=2; y++){
-document.write("<TD>",table[x][y],"</TD>");
-}
-document.write("</TR>");
+for (x = 1; x <= 3; x++) {
+  document.write("<TR><TD>", x, "</TD>");
+  for (y = 0; y <= 2; y++) {
+    document.write("<TD>", table[x][y], "</TD>");
+  }
+  document.write("</TR>");
 }
 document.write("</TABLE>");
 }
-part1 = new Array("Bro<sub>4</sub>","3",78);
-part2 = new Array("B","2",45);
-part3 = new Array("C","1","14");
-brakeParts = new Array("",part1,part2,part3);
-
-displayInventory(brakeParts);
-
+part1 = new Array("Bro<sub>4</sub>", "3", 78);
+part2 = new Array("B", "2", 45);
+part3 = new Array("C", "1", "14");
+brakeParts = new Array("", part1, part2, part3);
