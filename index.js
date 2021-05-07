5ca2d003d85356edbd6e15c203c6990bf1e8ba52
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-07 22:04:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,18 +13,3 @@
   }
   
 }
-function S1()
-{
-
-var x=['Sahand','bako'];
-var z=[25,45,50];
-var a=[50,55,60,70];
-
-document.write("name = "+x);
-document.write("Number = "+z);
-document.write("Num = "+a[0]);
-document.write("Number = "+a[1]);
-document.write("Num = "+a[2]);
-document.write("Num = "+a[3]);
-
-}
