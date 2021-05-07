e45fe95dac781fac62785ffb1133c2481c927525
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-08 00:11:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,17 +22,17 @@ function Sahand(){
   
 
 document.write("<TABLE BORDER=ON>");
-document.write("<TH>Item Number</TH><TH>Item Name</TH><TH>Model Number</TH><TH>Quantity</TH>");
-for (x = 1; x <= 3; x++) {
-  document.write("<TR><TD>", x, "</TD>");
-  for (y = 0; y <= 2; y++) {
+document.write("<TH>Sahand</TH><TH>Sahands</TH>");
+for(x = 1; x <=3; x++){ 
+  document.write("<TR><TD>",x, "</TD>");
+  for(y = 0; y<=2;y++){ 
     document.write("<TD>", table[x][y], "</TD>");
   }
   document.write("</TR>");
 }
 document.write("</TABLE>");
 }
-part1 = new Array("Bro<sub>4</sub>", "3", 78);
-part2 = new Array("B", "2", 45);
-part3 = new Array("C", "1", "14");
-brakeParts = new Array("", part1, part2, part3);
+
+part1 = new Array("Bro<sub>4</sub>","Bro<sub>3</sub>","Bro<sub>2</sub>");
+part2 = new Array("45","47","65");
+brakeParts = new Array("", part1, part2);
