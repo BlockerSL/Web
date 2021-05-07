0cdc743cffdfaaa28f4dec0eb11bc747b3fce80d
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-07 04:56:21 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,614 @@
+<?php
+$_GET["sahand"];
+
+if($_GET["sahand"]==="I")
+{
+	echo'<!DOCTYPE html>
+
+<html>
+<head>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <meta charset="utf-8">
+    <title>Welcomr</title>
+<h1 align="center">ێۆدید</h1>
+
+<br/>
+</head>
+<body class="p-3 mb-2 bg-dark text-white" >
+
+<center>
+
+  <style> 
+        table, th, td 
+        { 
+            border-collapse:collapse; 
+            border:1px solid black; 
+        } 
+        th, td 
+        { 
+            padding:5px; 
+        } 
+    </style> 
+    </head> 
+ 
+    <body> 
+ 
+    <table> 
+
+      <tr> 
+        <th style="background-color: #778899;">ناوی شێوگ</th> 
+        <th style="background-color: #778899;"> جۆرەکانی</th> 
+      </tr> 
+      <tr> 
+     
+        <td>ێۆدید</td> 
+        <td>Io<sup>-</sup> , Io<sub>2</sub><sup>-</sup> , Io<sub>3</sub><sup>-</sup> , Io<sub>4</sub><sup>-</sup></td> 
+      </tr> 
+<tr>
+
+<td>Io<sup>-</sup></td>
+<td> ژێر ێۆدیت </td>
+
+</tr>
+
+
+<tr>
+<td>Io<sub>2</sub><sup>-</sup></td>
+<td>ێۆدیت</td>
+</tr>
+   <tr>
+<td>Io<sub>3</sub><sup>-</sup></td>
+<td>ێۆدات</td>
+</tr>
+<tr>
+<td>Io<sub>4</sub><sup>-</sup></td>
+<td>پێر & ژوور  ێۆدات</td>
+</tr>
+
+    </table> 
+</center>
+
+</body>
+<style>
+
+body{
+
+background-attachment: fixed;
+background-position:center;
+background-repeat:no-repeat;
+
+}
+h1{
+border-radius:50px;
+box-shadow:0px 0px 10px 0px yellowgreen;
+
+}
+</style>
+</html>
+';
+}
+ if($_GET["sahand"]==="Cl")
+{
+	echo '<!DOCTYPE html>
+
+<html>
+<head>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <meta charset="utf-8">
+    <title>Welcomr</title>
+<h1 align="center">کلۆرید</h1>
+<br/>
+</head>
+<body class="p-3 mb-2 bg-dark text-white" >
+
+<center>
+
+  <style> 
+        table, th, td 
+        { 
+            border-collapse:collapse; 
+            border:1px solid black; 
+        } 
+        th, td 
+        { 
+            padding:5px; 
+        } 
+    </style> 
+    </head> 
+ 
+    <body> 
+ 
+    <table> 
+
+      <tr> 
+        <th style="background-color: #778899;">ناوی شێوگ</th> 
+        <th style="background-color: #778899;"> جۆرەکانی</th> 
+      </tr> 
+      <tr> 
+     
+        <td>کلۆرید</td> 
+        <td>Clo<sup>-</sup> , Clo<sub>2</sub><sup>-</sup> , Clo<sub>3</sub><sup>-</sup> , Clo<sub>4</sub><sup>-</sup></td> 
+      </tr> 
+<tr>
+
+<td>Clo<sup>-</sup></td>
+<td> ژێر کلۆریت </td>
+
+</tr>
+
+
+<tr>
+<td>Clo<sub>2</sub><sup>-</sup></td>
+<td>کلۆریت</td>
+</tr>
+   <tr>
+<td>Clo<sub>3</sub><sup>-</sup></td>
+<td>کلۆرات</td>
+</tr>
+<tr>
+<td>Clo<sub>4</sub><sup>-</sup></td>
+<td>پێر & ژوور کلۆرات</td>
+</tr>
+
+    </table> 
+</center>
+
+</body>
+<style>
+
+body{
+
+background-attachment: fixed;
+background-position:center;
+background-repeat:no-repeat;
+
+}
+h1{
+border-radius:50px;
+box-shadow:0px 0px 10px 0px yellowgreen;
+
+}
+</style>
+</html>
+';
+}
+
+if($_GET["sahand"]==="Br")
+{
+	echo '<!DOCTYPE html>
+
+<html>
+<head>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <meta charset="utf-8">
+    <title>Welcomr</title>
+<h1 align="center">بڕۆمید</h1>
+
+<br/>
+</head>
+<body class="p-3 mb-2 bg-dark text-white" >
+
+<center>
+
+  <style> 
+        table, th, td 
+        { 
+            border-collapse:collapse; 
+            border:1px solid black; 
+        } 
+        th, td 
+        { 
+            padding:5px; 
+        } 
+    </style> 
+    </head> 
+ 
+    <body> 
+ 
+    <table> 
+
+      <tr> 
+        <th style="background-color: #778899;">ناوی شێوگ</th> 
+        <th style="background-color: #778899;"> جۆرەکانی</th> 
+      </tr> 
+      <tr> 
+     
+        <td>بڕۆمید</td> 
+        <td>Bro<sup>-</sup> , Bro<sub>2</sub><sup>-</sup> , Bro<sub>3</sub><sup>-</sup> , Bro<sub>4</sub><sup>-</sup></td> 
+      </tr> 
+<tr>
+
+<td>Bro<sup>-</sup></td>
+<td> ژێر بڕۆمیت </td>
+
+</tr>
+
+
+<tr>
+<td>Bro<sub>2</sub><sup>-</sup></td>
+<td>بڕۆمیت</td>
+</tr>
+   <tr>
+<td>Bro<sub>3</sub><sup>-</sup></td>
+<td>برۆمات</td>
+</tr>
+<tr>
+<td>Bro<sub>4</sub><sup>-</sup></td>
+<td> پێر & ژوور بڕۆمات</td>
+</tr>
+
+    </table> 
+</center>
+
+</body>
+<style>
+
+body{
+
+background-attachment: fixed;
+background-position:center;
+background-repeat:no-repeat;
+
+}
+h1{
+border-radius:50px;
+box-shadow:0px 0px 10px 0px yellowgreen;
+
+}
+</style>
+</html>
+';
+}
+
+if($_GET["sahand"]==="F")
+{
+
+echo '<!DOCTYPE html>
+
+<html>
+<head>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <meta charset="utf-8">
+    <title>Welcomr</title>
+<h1 align="center">فلۆرید</h1>
+
+<br/>
+</head>
+<body class="p-3 mb-2 bg-dark text-white" >
+
+<center>
+
+  <style> 
+        table, th, td 
+        { 
+            border-collapse:collapse; 
+            border:1px solid black; 
+        } 
+        th, td 
+        { 
+            padding:5px; 
+        } 
+    </style> 
+    </head> 
+ 
+    <body> 
+ 
+    <table> 
+
+      <tr> 
+        <th style="background-color: #778899;">ناوی شێوگ</th> 
+        <th style="background-color: #778899;"> جۆرەکانی</th> 
+      </tr> 
+      <tr> 
+     
+        <td>فلۆرید</td> 
+        <td>F<sup>-</sup> 
+      </tr> 
+<tr>
+
+<td>F<sup>-</sup></td>
+<td> فلۆرید </td>
+
+</tr>
+
+
+    </table> 
+</center>
+
+</body>
+<style>
+
+body{
+
+background-attachment: fixed;
+background-position:center;
+background-repeat:no-repeat;
+
+}
+h1{
+border-radius:50px;
+box-shadow:0px 0px 10px 0px yellowgreen;
+
+}
+</style>
+</html>
+';
+
+}
+
+if($_GET["sahand"]==="Cn")
+{
+
+echo '<!DOCTYPE html>
+
+<html>
+<head>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <meta charset="utf-8">
+    <title>Welcomr</title>
+<h1 align="center">سیانید</h1>
+
+<br/>
+</head>
+<body class="p-3 mb-2 bg-dark text-white" >
+
+<center>
+
+  <style> 
+        table, th, td 
+        { 
+            border-collapse:collapse; 
+            border:1px solid black; 
+        } 
+        th, td 
+        { 
+            padding:5px; 
+        } 
+    </style> 
+    </head> 
+ 
+    <body> 
+ 
+    <table> 
+
+      <tr> 
+        <th style="background-color: #778899;">ناوی شێوگ</th> 
+        <th style="background-color: #778899;"> جۆرەکانی</th> 
+      </tr> 
+      <tr> 
+     
+        <td>سیانید</td> 
+        <td>CN<sup>-</sup> 
+      </tr> 
+<tr>
+
+<td>CN<sup>-</sup></td>
+<td> سیانید </td>
+
+</tr>
+
+
+    </table> 
+</center>
+
+</body>
+<style>
+
+body{
+
+background-attachment: fixed;
+background-position:center;
+background-repeat:no-repeat;
+
+}
+h1{
+border-radius:50px;
+box-shadow:0px 0px 10px 0px yellowgreen;
+
+}
+</style>
+</html>
+';
+
+}
+
+
+if($_GET["sahand"]==="Oh")
+{
+
+echo '<!DOCTYPE html>
+
+<html>
+<head>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <meta charset="utf-8">
+    <title>Welcomr</title>
+<h1 align="center">هایدرۆکسید</h1>
+
+<br/>
+</head>
+<body class="p-3 mb-2 bg-dark text-white" >
+
+<center>
+
+  <style> 
+        table, th, td 
+        { 
+            border-collapse:collapse; 
+            border:1px solid black; 
+        } 
+        th, td 
+        { 
+            padding:5px; 
+        } 
+    </style> 
+    </head> 
+ 
+    <body> 
+ 
+    <table> 
+
+      <tr> 
+        <th style="background-color: #778899;">ناوی شێوگ</th> 
+        <th style="background-color: #778899;"> جۆرەکانی</th> 
+      </tr> 
+      <tr> 
+     
+        <td> هایدرۆکسید </td> 
+        <td>OH<sup>-</sup> 
+      </tr> 
+<tr>
+
+<td>OH<sup>-</sup></td>
+<td> هایدرۆکسید </td>
+
+</tr>
+
+
+    </table> 
+</center>
+
+</body>
+<style>
+
+body{
+
+background-attachment: fixed;
+background-position:center;
+background-repeat:no-repeat;
+
+}
+h1{
+border-radius:50px;
+box-shadow:0px 0px 10px 0px yellowgreen;
+
+}
+</style>
+</html>
+';
+
+}
+
+
+if($_GET["sahand"]==="N")
+{
+	
+	echo '<!DOCTYPE html>
+
+<html>
+<head>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <meta charset="utf-8">
+    <title>Welcomr</title>
+<h1 align="center">نیترید</h1>
+
+<br/>
+</head>
+<body class="p-3 mb-2 bg-dark text-white" >
+
+<center>
+
+  <style> 
+        table, th, td 
+        { 
+            border-collapse:collapse; 
+            border:1px solid black; 
+        } 
+        th, td 
+        { 
+            padding:5px; 
+        } 
+    </style> 
+    </head> 
+ 
+    <body> 
+ 
+    <table> 
+
+      <tr> 
+        <th style="background-color: #778899;">ناوی شێوگ</th> 
+        <th style="background-color: #778899;"> جۆرەکانی</th> 
+      </tr> 
+      <tr> 
+     
+        <td> نیتریت </td> 
+        <td>NO<sub>2</sub><sup>-</sup> 
+      </tr> 
+<tr>
+
+<td>NO<sub>2</sub><sup>-</sup> 
+
+<td> نیتریت </td>
+
+</tr>
+<tr>
+
+<td>NO<sub>3</sub><sup>-</sup> 
+
+<td> نیترات</td>
+
+
+
+
+
+
+</tr>
+
+
+
+    </table> 
+</center>
+
+</body>
+<style>
+
+body{
+
+background-attachment: fixed;
+background-position:center;
+background-repeat:no-repeat;
+
+}
+h1{
+border-radius:50px;
+box-shadow:0px 0px 10px 0px yellowgreen;
+
+}
+</style>
+</html>
+';
+	
+	
+}
+
+
+
+if(isset($_GET['Get']))
+{
+	
+
+	
+	echo"Login";
+	
+	
+	
+}
+
+
+
+ 
+ ?>
