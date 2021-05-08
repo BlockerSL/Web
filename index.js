a9ea0cd1214649eb0f79ea70e769c5b18de38119
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-08 06:04:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,27 +12,22 @@
   else{
     alert("fail");
   }
-  
-}
 
+if(Tag=="Br"){
+ 
 
+  open("https://sahand-chemist-br.netlify.app/");
 
-
-function Sahand(){
-  
-
-document.write("<TABLE BORDER=ON>");
-document.write("<TH>Sahand</TH><TH>Sahands</TH>");
-for(x = 1; x <=3; x++){ 
-  document.write("<TR><TD>",x, "</TD>");
-  for(y = 0; y<=2;y++){ 
-    document.write("<TD>", table[x][y], "</TD>");
+ 
   }
-  document.write("</TR>");
-}
-document.write("</TABLE>");
+  
+  else{
+    alert("fail");
+  }
+  
+
+
+  
 }
 
-part1 = new Array("Bro<sub>4</sub>","Bro<sub>3</sub>","Bro<sub>2</sub>");
-part2 = new Array("45","47","65");
-brakeParts = new Array("", part1, part2);
+
