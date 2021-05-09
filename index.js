ce0b84e30418174318e1568e6b4e6511f7b17743
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-09 16:37:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,73 +1,13 @@
-    function sahand()
+function sahand()
 {
-  var  Tag=document.getElementById("Tag").value;
-  if(Tag=="Cl"){
- 
+  var Tag = document.getElementById("Tag").value;
+  if (Tag == "Cl") {
 
- open("https://sahand-chemist-i.netlify.app", "_self");
-
- 
+  window.open('https://sahand-chemist-i.netlify.app');
   }
-
-if(Tag=="Br"){
- 
-
-  open("https://sahand-chemist-br.netlify.app/");
-
- 
-  }
-
-if(Tag=="I"){
- 
-
-  open("https://sahand-chemist-i1.netlify.app/");
-
- 
+  else {
+    alert("incorect Name");
+    
   }
   
-
-if(Tag=="F"){
- 
-
-  open("https://sahand-chemist-f.netlify.app/");
-
- 
-  }
-  
-
- if(Tag=="Cn"){
- 
-
-  open("https://sahand-chemist-cn.netlify.app/");
-
- 
-  }
-
-if(Tag=="Oh"){
- 
-
-  open("https://sahand-chemist-oh.netlify.app/");
-
- 
-  }
-
-if(Tag=="N"){
- 
-
-  open("https://sahand-chemist-n.netlify.app/");
-
- 
-  }
-
-  
-  else{
-    alert("fail");
-  }
-
-  
- 
-
-  
-}
-
 
