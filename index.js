60d4def89048f6ea2c89226cbb53a699d9865e28
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-09 17:00:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-Tag = document.getElementById("Tag").value;
+function sahand()
+{
+  var Tag = document.getElementById("Tag").value;
   if (Tag == "Cl") {
 
-  window.open('https://sahand-chemist-i.netlify.app','_blank');
+  window.open('https://sahand-chemist-i.netlify.app', '_blank');
   }
   else if (Tag == "Br"){
-    window.open('https://sahand-chemist-br.netlify.app/','_blank')
+    window.open('https://sahand-chemist-br.netlify.app', '_blank');
   }
   
   else {
     alert("incorect Name");
     
   }
-  
-  
+  }
