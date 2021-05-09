a1778645c6068fed0f146ca793bb5ab2ec222660
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-09 16:48:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function sahand()
   var Tag = document.getElementById("Tag").value;
   if (Tag == "Cl") {
 
-  window.open('https://sahand-chemist-i.netlify.app', '_self');
+  window.open('https://sahand-chemist-i.netlify.app', '_blank');
   }
   else {
     alert("incorect Name");
