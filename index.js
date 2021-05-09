04bb2007b4a300f806a4a5e191f2cefbc3c26edf
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-09 17:06:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,27 @@ function sahand()
   else if (Tag == "Br"){
     window.open('https://sahand-chemist-br.netlify.app', '_blank');
   }
+  else if (Tag == "I") {
+    window.open('https://sahand-chemist-i1.netlify.app', '_blank');
+  }
   
+  else if (Tag == "F") {
+    window.open('https://sahand-chemist-f.netlify.app', '_blank');
+  }
+  
+  else if (Tag == "N") {
+    window.open('https://sahand-chemist-n.netlify.app', '_blank');
+  }
+  
+  else if (Tag == "Oh") {
+    window.open('https://sahand-chemist-oh.netlify.app', '_blank');
+  }
+  else if (Tag == "Cn") {
+    window.open('https://sahand-chemist-cn.netlify.app', '_blank');
+  }
   else {
     alert("incorect Name");
     
   }
-  }
+  
+}
