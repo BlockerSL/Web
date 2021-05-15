0652ae72c9e8f472177e9e87f920b9bfb7cf83cf
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-16 01:24:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function sahand()
   var Tag = document.getElementById("Tag").value;
   if (Tag == "Cl") {
 
-  window.open("https://sahand-chemist-i.netlify.app", "Welcome");
+  window.open('https://sahand-chemist-i.netlify.app', '_blank');
   }
   else if (Tag == "Br"){
     window.open('https://sahand-chemist-br.netlify.app', '_blank');
